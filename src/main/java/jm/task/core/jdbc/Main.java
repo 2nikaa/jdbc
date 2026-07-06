9ec0c4aefb087402cf8362d9e6a4984131c6efa8
2nikaa <nik.nikonov.28@mail.ru>
2026-07-06 08:34:22 +0300

    done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+
+        userDao.saveUser("nikita", "nikonov", (byte) 24);
+        userDao.saveUser("vika", "sus", (byte) 21);
+        userDao.saveUser("elon", "musk", (byte) 99);
+        userDao.saveUser("vova", "putin", (byte) 3);
+
+        userDao.getAllUsers().forEach(System.out::println);
+
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,8 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
@@ -10,26 +14,70 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                "name VARCHAR(64) NOT NULL, " +
+                "last_name VARCHAR(64) NOT NULL, " +
+                "age TINYINT NOT NULL); ";
+        executeSql(sql, "Не получилось создать таблицу");
     }
 
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS users";
+        executeSql(sql, "Не удалось удалить таблицу");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)";
+        executeSql(sql, "Не удалось добавить пользователя", name, lastName, age);
+        System.out.printf("User с именем — %s добавлен в базу данных\n", name);
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM users WHERE id = ?";
+        executeSql(sql, "Не получилось удалить пользователя по id", id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT * FROM users";
+        List<User> users  = new ArrayList<>();
+
+        try (Connection connection = Util.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    User user = new User();
+
+                    user.setId(resultSet.getLong("id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setLastName(resultSet.getString("last_name"));
+                    user.setAge(resultSet.getByte("age"));
+
+                    users.add(user);
+                }
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("Не получилось получить таблицу всех user'ов", e);
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE users";
+        executeSql(sql, "Не удалось удалить всех пользователей");
+    }
 
+    private void executeSql (String sql, String errorMessage, Object... params) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            for (int i = 0; i < params.length; i++) {
+                statement.setObject(i + 1, params[i]);
+            }
+            statement.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(errorMessage, e);
+        }
     }
 }
